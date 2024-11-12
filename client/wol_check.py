f4204f5c382e613fe1dc4cff71dbd5b9825632b5
eugene2021 <eugene.wu@canonical.com>
2024-11-12 17:53:01 +0800

    Use logging for wol_check and wol_client. Use sys.exit instead of return.

--- client/wol_check.py
+++ client/wol_check.py
@@ -3,13 +3,14 @@
 import subprocess
 import re
 import argparse
+import logging
+import sys
 
 
 def get_timestamp(file):
     with open(file, "r") as f:
         saved_timestamp = float(f.read())
-        print("saved_timestamp:")
-        print(saved_timestamp)
+        logging.info(f"saved_timestamp: {saved_timestamp}")
     return saved_timestamp
 
 
@@ -24,6 +25,8 @@ def extract_timestamp(log_line):
 
 
 def get_suspend_boot_time(type):
+    # get the time stamp of the system resume from suspend for s3
+    # or boot up for s5
     command = ["journalctl", "-b", "0", "--output=short-unix"]
     result = subprocess.run(command, capture_output=True, text=True)
     logs = result.stdout.splitlines()
@@ -32,26 +35,22 @@ def get_suspend_boot_time(type):
 
     if type == "s3":
         for log in reversed(logs):
-            # print("log---:", log)
             if r"suspend exit" in log:
-                print(log)
+                logging.debug(log)
                 latest_system_back_time = extract_timestamp(log)
-                print("suspend time:", latest_system_back_time)
-                # print(latest_system_back_time)
+                logging.info(f"suspend time: {latest_system_back_time}")
                 return latest_system_back_time
     elif type == "s5":
+        # the first line of system boot up
         log = logs[0]
         latest_system_back_time = extract_timestamp(log)
-        print("boot_time:", latest_system_back_time)
+        logging.info(f"boot_time: {latest_system_back_time}")
         return latest_system_back_time
     else:
-        print("Invalid type. Please use s3 or s5.")
-        return None
+        sys.exit("Invalid type. Please use s3 or s5.")
 
-    # if not found "suspend exit"，return False
     if latest_system_back_time is None:
-        print("cannot find 'suspend exit' or boot time in log")
-        return None
+        sys.exit("cannot find 'suspend exit' or boot time in kernel log")
 
 
 def main():
@@ -60,9 +59,7 @@ def main():
 
     parser.add_argument("--interface", required=True,
                         help="The network interface to use.")
-    # parser.add_argument("--waketype", default="g",
-    # help="Type of wake operation.")
-    parser.add_argument("--powertype", type=str, help="Type of s3 or s5.")
+    parser.add_argument("--powertype", type=str, help="Waked from s3 or s5.")
     parser.add_argument("--timestamp_file", type=str,
                         help="The file to store the timestamp of test start.")
     parser.add_argument("--delay", type=int, default=60,
@@ -72,34 +69,41 @@ def main():
 
     args = parser.parse_args()
 
+    logging.basicConfig(
+        level=logging.DEBUG,
+        stream=sys.stdout,
+        format="%(levelname)s: %(message)s",
+    )
+
+    logging.info("Wake on LAN log check test started.")
+
     interface = args.interface
-    # waketype = args.waketype
     powertype = args.powertype
     timestamp_file = args.timestamp_file
     delay = args.delay
     max_retries = args.retry
 
-    print(f"Interface: {interface}")
-    print(f"PowerType: {powertype}")
+    logging.info(f"Interface: {interface}")
+    logging.info(f"PowerType: {powertype}")
 
     test_start_time = float(get_timestamp(timestamp_file))
     system_back_time = float(get_suspend_boot_time(powertype))
 
     time_difference = system_back_time - test_start_time
 
-    print("time difference:", time_difference)
+    logging.info(f"time difference: {time_difference}")
 
-    # system_back_time - test_start_time > 1.5*max_retries*delay
-    # meanse the system was bring up by rtc other than Wake-on-lan
-    if time_difference > 1.5*max_retries*delay:
-        print("time difference is greater than 1.5*max_retries*delay")
-        return False
+    # system_back_time - test_start_time > 1.5*max_retries*delay which meanse
+    # the system was bring up by rtc other than Wake-on-lan
+    expect_time_range = 1.5*max_retries*delay
+    if time_difference > expect_time_range:
+        sys.exit(f"Time difference is {time_difference} greater than "
+                 f"1.5*delay*retry {expect_time_range}")
     elif time_difference < 0:
-        print("time difference is less than 0")
-        return False
+        sys.exit("Time difference is less than 0.")
     else:
-        print("time difference is within the range")
-        return True
+        logging.info("Wake-on-lan workes well.")
+        sys.exit(True)
 
 
 if __name__ == "__main__":
