45d6d336941590107dc76c9bda00e999a7b3e8ee
eugene2021 <eugene.wu@canonical.com>
2025-02-25 19:21:46 +0800

    use more specific 4xx 5xx status code in wol_server

--- server/wol_server.py
+++ server/wol_server.py
@@ -11,7 +11,7 @@ from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
-LOG_LEVEL = 'DEBUG'
+LOG_LEVEL = "DEBUG"
 logging.basicConfig(level=LOG_LEVEL)
 logger = logging.getLogger(__name__)
 
@@ -20,8 +20,7 @@ logger = logging.getLogger(__name__)
 async def testing(wol_request: dict):
     try:
         ret_server = tasker_main(wol_request)
-        return JSONResponse(content=jsonable_encoder(ret_server),
-                            status_code=200)
+        return ret_server
     except Exception as e:
         logger.critical(repr(e))
 
@@ -33,21 +32,21 @@ def send_wol_command(Wol_Info: dict):
     wake_type = Wol_Info["wake_type"]
 
     command_dict = {
-                    "g": f"wakeonlan {dut_mac}",
-                    "a": f"ping {dut_ip}",
-                   }
+        "g": "wakeonlan {}".format(dut_mac),
+        "a": "ping {}".format(dut_ip),
+    }
 
     try:
-        logger.debug(f"Wake on lan command: {command_dict[wake_type]}")
+        logger.debug("Wake on lan command: {}".format(command_dict[wake_type]))
         output = subprocess.check_output(shlex.split(command_dict[wake_type]))
         logger.debug({output})
 
     except subprocess.CalledProcessError as e:
-        logger.error(f"Error occurred in tasker_main: {e}")
+        logger.error("Error occurred in tasker_main: {}".format(e))
         return False
 
     except KeyError as e:
-        logger.error(f"Error occurred in tasker_main: {e}")
+        logger.error("Error occurred in tasker_main: {}".format(e))
         return False
 
     return True
@@ -57,53 +56,59 @@ def tasker_main(request: dict) -> dict:
 
     try:
         # Extracting necessary fields from the request
-        dut_ip = request.get('DUT_IP')
-        delay = request.get('delay')
+        dut_ip = request.get("DUT_IP")
+        delay = request.get("delay")
 
         if not dut_ip or delay is None:
             logger.error("Missing required fields: DUT_IP or delay")
-            return {'result': 'error', 'message': 'Missing required fields'}
+            return JSONResponse(
+                content=jsonable_encoder({"message": "Missing required fields"}),
+                status_code=400,
+            )
 
-        logger.info(f"Received request: {request}")
-        logger.info(f"DUT_IP: {dut_ip}")
+        logger.info("Received request: {}".format(request))
+        logger.info("DUT_IP: {}".format(dut_ip))
 
         # Starting the task in a separate thread
         thread = threading.Thread(target=run_task, args=(request, delay))
         thread.start()
 
         # Returning success response
-        return {'result': 'success'}
+        return JSONResponse(
+            content=jsonable_encoder({"message": "success"}), status_code=200
+        )
 
     except Exception as e:
-        logger.exception(f"Error occurred while processing the request: {e}")
-        return {'result': 'error', 'message': str(e)}
+        logger.exception("Error occurred while processing the request: {}".format(e))
+        return JSONResponse(
+            content=jsonable_encoder({"message": str(e)}), status_code=500
+        )
 
 
 def is_pingable(ip_address):
     try:
         #  use ping command to ping the host
         command = ["ping", "-c", "1", "-W", "1", ip_address]
-        output = subprocess.check_output(command, stderr=subprocess.STDOUT,
-                                         universal_newlines=True)
-        logger.debug(f"ping: {output}")
+        output = subprocess.check_output(
+            command, stderr=subprocess.STDOUT, universal_newlines=True
+        )
+        logger.debug("ping: {}".format(output))
         return True
-    except subprocess.CalledProcessError:
+    except subprocess.CalledProcessError as e:
         # print("ping:", output)
-        logger.debug("An error occurred while ping the DUT: str{e}")
+        logger.debug("An error occurred while ping the DUT: str{}".format(e))
         return False
 
 
 def run_task(data, delay):
 
-    # dut_mac = data['DUT_MAC']
-    dut_ip = data['DUT_IP']
-    delay = data['delay']
-    retry_times = data['retry_times']
-    # wake_type = data['wake_type']
+    dut_ip = data["DUT_IP"]
+    delay = data["delay"]
+    retry_times = data["retry_times"]
 
     for attempt in range(retry_times):
         # logger.info("threading:", dut_mac)
-        logger.debug(f"retry times: {attempt}")
+        logger.debug("retry times: {}".format(attempt))
         time.sleep(delay)
 
         try:
@@ -117,14 +122,13 @@ def run_task(data, delay):
             time.sleep(delay)
             # ping dut
             if is_pingable(dut_ip):
-                logger.info(f"{dut_ip} is pingable, the DUT is back")
-                # logger.info("ping DUT to see if it had been waked up")
+                logger.info("{} is pingable, the DUT is back".format(dut_ip))
                 return True
             else:
-                logger.info(f"{dut_ip} is NOT pingable, the DUT is not back.")
+                logger.info("{} is NOT pingable, the DUT is not back.".format(dut_ip))
 
         except Exception as e:
-            logger.error(f"Error occurred in tasker_main: {e}")
+            logger.error("Error occurred in tasker_main: {}".format(e))
 
         # retry finished
     return False
@@ -132,10 +136,10 @@ def run_task(data, delay):
 
 if __name__ == "__main__":
     req = {
-          "DUT_MAC": "00:00:00:00:00",
-          "DUT_IP": "127.0.0.1",
-          "delay": 60,
-          "retry_times": 5,
-          "wake_type": "g"
-          }
+        "DUT_MAC": "00:00:00:00:00",
+        "DUT_IP": "127.0.0.1",
+        "delay": 60,
+        "retry_times": 5,
+        "wake_type": "g",
+    }
     r1 = tasker_main(request=req)
